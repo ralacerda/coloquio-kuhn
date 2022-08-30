4ef46bdf74cda5e64e44ddc862b13bfcafdbeff6
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-08-30 18:41:28 -0300

    programacao com detalhes

--- src/data/programacao.js
+++ src/data/programacao.js
@@ -1,28 +1,181 @@
 module.exports = [
   {
-    nome: "QUINTA-FEIRA, 29 de Setembro de 2022",
+    nome: "Quinta-Feira, 29 de Setembro",
     eventos: [
-      { horario: "08:00", nome: "Inscrições / Credenciamento" },
-      { horario: "09:30", nome: "Abertura" },
-      { horario: "10:00", nome: "Conferência de abertura" },
+      { horario: "08:00 - 09:30", nome: "Inscrições / Credenciamento" },
+      {
+        horario: "09:30",
+        titulo: "Abertura",
+        palestras: [
+          {
+            autores: [
+              "Oswaldo Gonçalves (Coord. ICHSA/Unicamp)",
+              "Gustavo Silvano Batista (UFPI)",
+              "Peter Schulz (ICHSA/Unicamp)",
+            ],
+          },
+        ],
+      },
+      {
+        horario: "10:00",
+        titulo: "Conferência de abertura",
+
+        coord: "Peter Schulz",
+        palestras: [
+          { titulo: "Kuhn epistemólogo", autores: ["Silvio Seno Chibeni"] },
+        ],
+      },
       { horario: "12:00", nome: "Almoço" },
-      { horario: "14:00", nome: "Mesa 1" },
+      {
+        horario: "14:00",
+        nome: "Mesa 1",
+        titulo: "O pensamento de Kuhn na filosofia da ciência hoje",
+        coord: "Eduardo Marandola Jr. (ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo: "O legado de Kuhn para a historiografia da ciência",
+            autores: ["Mauro Lúcio Leitão Condé (UFMG)"],
+          },
+          {
+            titulo:
+              "A trajetória da física para as humanidades e uma pergunta final",
+            autores: ["Peter Schulz (ICHSA/Unicamp)"],
+          },
+        ],
+      },
       { horario: "16:00", nome: "Intervalo" },
-      { horario: "16:30", nome: "Sessão temática 1" },
+      {
+        horario: "16:30",
+        nome: "Sessão temática 1",
+        titulo: "Repercussões e fecundidade do conceito de “paradigma”",
+        coord: "Antonio Bernardes (UFF/ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo: "O paradigma da psicologia",
+            autores: [
+              "Gustavo Costa Bahia Rocha (PUC-MG)",
+              "Rodolfo Victor Cancio Evangelista (ICHSA/Unicamp)",
+            ],
+          },
+          {
+            titulo: "Ladrière, Kuhn e os paradigmas",
+            autores: ["Maycon Silva Santos (UFPI)"],
+          },
+          {
+            titulo:
+              "Fabulação feminista: a mudança de paradigma na ficção escrita por mulheres",
+            autores: [
+              "Tais Alves Teixeira (ICHSA/Unicamp)",
+              "Fernanda Nogueira Viana (IG/Unicamp)",
+              "Tiago Rodrigues Moreira (IG/Unicamp)",
+            ],
+          },
+        ],
+      },
       { horario: "18:00", nome: "Encerramento" },
     ],
   },
   {
-    nome: "SEXTA-FEIRA, 30 de Setembro de 2022",
+    nome: "Quinta-feira, 30 de Setembro",
     eventos: [
-      { horario: "08:00", nome: "Sessão temática 2" },
-      { horario: "09:30", nome: "Intervalo" },
-      { horario: "10:00", nome: "Mesa 2" },
-      { horario: "12:00", nome: "Almoço" },
-      { horario: "14:00", nome: "Mesa 3" },
-      { horario: "16:00", nome: "Intervalo" },
-      { horario: "16:30", nome: "Sessão temática 3" },
-      { horario: "18:00", nome: "Encerramento" },
+      {
+        horario: "08:00",
+        nome: "Sessão temática 2",
+        titulo: "Repercussões kuhnianas nas ciências I",
+        coord: "Sandra Francisca Bezerra Gemma (ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo: "A tradução na Filosofia de Thomas Kuhn",
+            autores: ["Francidilso Silva do Nascimento (UFPI)"],
+          },
+          {
+            titulo:
+              "A estrutura das revoluções científicas e o ensino de ciências",
+            autores: [
+              "John Lucas Alves Fernandes da Silva (Unifesp)",
+              "Denilson Soares Cordeiro",
+            ],
+          },
+        ],
+      },
+
+      {
+        horario: "09:00",
+        nome: "Intervalo",
+      },
+      {
+        horario: "09:15",
+        nome: "Mesa 2",
+        titulo: "Filosofia de Thomas Kuhn",
+        coord: "Mauro Cardoso Simões (ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo:
+              "Relativismo ou pragmatismo nas controvérsias científicas kuhnianas?",
+            autores: ["Robinson Guitarrari (UFRRJ)"],
+          },
+          { titulo: "Os usos da história", autores: ["Paulo Pirozelli (USP)"] },
+          {
+            titulo: "Seria Thomas Kuhn um hermeneuta?",
+            autores: ["Gustavo Silvano Batista (UFPI)"],
+          },
+        ],
+      },
+      {
+        horario: "12:15",
+        nome: "Almoço",
+      },
+      {
+        horario: "14:00",
+        nome: "Mesa 3",
+        titulo: "Comunidades científicas e paradigmas nas ciências",
+        coord: "Roberto Donato da Silva Jr. (ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo:
+              "O papel das comunidades científicas na construção do conceito de informação no século XX",
+            autores: ["Cibelle Celestino Silva (UFSCar)"],
+          },
+          {
+            titulo: "Kuhn e a crise na astronomia quinhentista",
+            autores: ["Fátima Évora (IFCH/Unicamp)"],
+            aConfirmar: true,
+          },
+        ],
+      },
+      {
+        horario: "16:00",
+        nome: "Intervalo",
+      },
+      {
+        horario: "16:30",
+        nome: "Sessão temática 3",
+        titulo: "Repercussões kuhnianas nas ciências II",
+        coord: "Márcio Barreto (ICHSA/Unicamp)",
+        palestras: [
+          {
+            titulo: "Imagens, anomalias e revoluções: Farewell to Big Bang",
+            autores: [
+              "Marcos Cesar Danhoni Neves (UEM)",
+              "Josie Agatha Parrilha da Silva (UEPG)",
+            ],
+          },
+          {
+            titulo:
+              "Em busca da luz: uma análise kuhniana da óptica pré-newtoniana",
+            autores: ["Lucas de Abreu Kasprik (UFPR)"],
+          },
+          {
+            titulo:
+              "Mudanças paradigmáticas do treinamento em altitude e hipóxia à luz da estrutura das revoluções científicas",
+            autores: ["Marcelo Papoti (USP)"],
+          },
+        ],
+      },
+      {
+        horario: "18:00",
+        nome: "Encerramento",
+      },
     ],
   },
 ];
