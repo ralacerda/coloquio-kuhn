473792e5dc27156d1025230460b7f3d18e92672d
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-08-31 11:17:21 -0300

    Nova página de trabalhos

---
+++ src/data/trabalhos.js
@@ -0,0 +1,54 @@
+module.exports = [
+  {
+    titulo: "A estrutura das revoluções científicas e o ensino de ciências",
+    autores: [
+      "John Lucas Alves Fernandes da Silva (Unifesp)",
+      "Denilson Soares Cordeiro",
+    ],
+  },
+  {
+    titulo: "A tradução na Filosofia de Thomas Kuhn",
+    autores: ["Francidilso Silva do Nascimento (UFPI)"],
+  },
+  {
+    titulo: "Em busca da luz: uma análise kuhniana da óptica pré-newtoniana",
+    autores: ["Lucas de Abreu Kasprik (UFPR)"],
+  },
+  {
+    titulo:
+      "Fabulação feminista: a mudança de paradigma na ficção escrita por mulheres",
+    autores: [
+      "Tais Alves Teixeira (ICHSA/Unicamp)",
+      "Fernanda Nogueira Viana (IG/Unicamp)",
+      "Tiago Rodrigues Moreira (IG/Unicamp)",
+    ],
+  },
+  {
+    titulo: "Imagens, anomalias e revoluções: Farewell to Big Bang",
+    autores: [
+      "Marcos Cesar Danhoni Neves (UEM)",
+      "Josie Agatha Parrilha da Silva (UEPG)",
+    ],
+  },
+  {
+    titulo: "Ladrière, Kuhn e os paradigmas",
+    autores: ["Maycon Silva Santos (UFPI)"],
+  },
+
+  {
+    titulo:
+      "Mudanças paradigmáticas do treinamento em altitude e hipóxia à luz da estrutura das revoluções científicas",
+    autores: [
+      "Marcelo Papoti (USP)",
+      "Fúlvia de Barros Manchado Gobatto (Unicamp)",
+      "Claudio Alexandre Gobatto (Unicamp)",
+    ],
+  },
+  {
+    titulo: "O paradigma da psicologia",
+    autores: [
+      "Gustavo Costa Bahia Rocha (PUC-MG)",
+      "Rodolfo Victor Cancio Evangelista (ICHSA/Unicamp)",
+    ],
+  },
+];
