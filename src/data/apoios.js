7edc7e33daf176cfed4fde5ed5cf85b067fbc95e
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-26 15:57:17 -0300

    Nomes para todos os links

--- src/data/apoios.js
+++ src/data/apoios.js
@@ -4,11 +4,13 @@ module.exports = {
       siteLink:
         "https://www.fca.unicamp.br/portal/pt-br/posgrad/posgrad-programas/posgrad-cursos-ichsa/288-programa-de-pos-graduacao-interdisciplinar-em-ciencias-humanas-e-sociais-aplicadas.html",
       logoLink: "/img/logos/ichsa.webp",
+      nameLink: "ichsa",
     },
 
     {
       siteLink: "https://sigaa.ufpi.br/sigaa/public/programa/portal.jsf?id=956",
       logoLink: "/img/logos/ppgfil.webp",
+      nameLink: "ppgfil",
     },
   ],
 
@@ -16,27 +18,32 @@ module.exports = {
     {
       siteLink: "https://fenomenologiaegeografia.wordpress.com",
       logoLink: "/img/logos/nomear.webp",
+      nameLink: "nomear",
     },
 
     {
       siteLink: "https://www.fca.unicamp.br/lagerr/",
       logoLink: "/img/logos/lagerr.webp",
+      nameLink: "lagerr",
     },
 
     {
       siteLink: "https://geografiahumanista.wordpress.com/about/",
       logoLink: "/img/logos/folie.webp",
+      nameLink: "folie",
     },
   ],
   local: [
     {
       siteLink: "https://www.fca.unicamp.br/portal/pt-br/",
       logoLink: "/img/logos/fca.webp",
+      nameLink: "fca",
     },
 
     {
       siteLink: "https://www.unicamp.br/unicamp/",
       logoLink: "/img/logos/unicamp.webp",
+      nameLink: "unicamp",
     },
   ],
 };
--- src/shortcodes.js
+++ src/shortcodes.js
@@ -9,8 +9,8 @@ module.exports = {
   button(icon, link) {
     return `
     <a href="${link}" target="_blank" class="p-3 block w-fit mx-auto hover:bg-hover leading-tight rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out bg-green"
-  title="${icon}">
-    <img class="w-8 h-8 inline" src="/coloquio-kuhn/img/icons/${icon}.svg" alt=""/>
+  >
+    <img class="w-8 h-8 inline" src="/coloquio-kuhn/img/icons/${icon}.svg" alt="Link para ${icon}"/>
 
 
   </a>
