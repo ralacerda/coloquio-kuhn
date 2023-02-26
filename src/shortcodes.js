c8df4b72ae518559527bda176ff0165a9beced82
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-02-26 14:54:12 -0300

    removido botão desnecessário para link

--- src/shortcodes.js
+++ src/shortcodes.js
@@ -8,15 +8,11 @@ module.exports = {
   },
   button(icon, link) {
     return `
-    <a href="${link}" target="_blank">
-    <button
-    type="button"
-    class="p-3 mx-3  hover:bg-hover leading-tight rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out bg-green"
+    <a href="${link}" target="_blank" class="p-3 mx-3  hover:bg-hover leading-tight rounded-full shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out bg-green"
   >
     <img class="w-8 h-8" src="/coloquio-kuhn/img/icons/${icon}.svg" />
 
 
-  </button>
   </a>
     `;
   },
